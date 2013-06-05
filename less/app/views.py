7ff4e553998369c326847e800a637d02387bccd9
Dou Xin <wq-doux@cn.fujitsu.com>
2013-06-05 11:47:51 +0800

    finish the show of each host's detail information
    Signed-off-by: Dou Xin <wq-doux@cn.fujitsu.com>

--- less/app/views.py
+++ less/app/views.py
@@ -2,17 +2,12 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 import os
+import re
 import paramiko
 import threading
 
 hosts_info = {}
-
-def setuphosts_info(host_list):
-    global hosts_info
-    length = len(host_list)
-    for i in range(length):
-        hosts_info[host_list[i]] = ""
-
+pwd = os.path.dirname(os.path.abspath(__file__))
 
 def gethostinfo(host,lock):
     global hosts_info
@@ -33,15 +28,13 @@ def gethostinfo(host,lock):
 
 def myapp(request):
     global hosts_info
+    global pwd
     threads = []
     lock = threading.RLock()
-    filename = os.path.join(os.path.dirname(os.path.abspath(__file__)),"hosts")
+    filename = os.path.join(pwd,"hosts")
     f = open(filename)
     host_list = f.read().splitlines()
-    print host_list
     f.close()
-    setuphosts_info(host_list)
-    print hosts_info
     for host in host_list:
         t = threading.Thread(target=gethostinfo, args=(host,lock))
         threads.append(t)
@@ -52,25 +45,86 @@ def myapp(request):
     return render_to_response("index.html",{"hosts_info":hosts_info, "host_list":host_list})
 
 
+def dealgrub(buf, flag):
+    if flag == "grub" :
+        keywd = "kernel"
+    else :
+        keywd = "linux"
+    kernel = []
+    klist = re.findall("^\s+|\t+%s.*?\n"%keywd, buf)
+    for line in klist :
+        alist = re.split("\s+|\t+", line)
+        kernel.append(alist[2].lstrip("/"))
+    kernel = list(set(kernel))
+    return kernel
+
+
+def anawho(w_list):
+    global pwd
+    result = {}
+    m_dict = {}
+    filename = os.path.join(pwd,"members")
+    f = open(filename)
+    m_list = f.read().splitlines()
+    f.close()
+    for line in m_list :
+        alist = re.split("\s+",line)
+        m_dict[alist[1]] = alist[0]
+    w_list = list(set(w_list))
+    for line in w_list :
+        m = re.search("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})",line);
+        if m :
+            if m.group(1) in m_dict.keys() :
+                result[m.group(1)] = m_dict[m.group(1)]
+            else :
+                result[m.group(1)] = "Unknown"
+    return result
+
+
 def detail(request, host):
+    global hosts_info
     html = ""
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host, 22, "root", "ltech2")
-    sin,sout,serr=ssh.exec_command("who")
-    alist = sout.read().splitlines()
-    html += "<p align='center'><strong>who</strong></p>"
+    html += "<center><b>DETAILS</b></center>"
     html += "<table frame='hsides' width='700' align='center'>"
-    if alist :
-        for line in alist:
-            html += "<tr>"
-            import re
-            blist = re.split("\s+", line)
-            for item in blist:
-                html += "<td>"+item+"</td>"
-            html += "</tr>"
+    # CURRENT
+    html += "<tr class='detail1'><td><b>CURRENT</b></td><td>" + hosts_info[host] + "</td></tr>"
+    # BOOT
+    html += "<tr class='detail2'><td><b>BOOT</b></td><td>"
+    sin,sout,serr=ssh.exec_command("df | grep /boot | awk '{print $1}'")
+    boot = sout.read()
+    if boot :
+        html += boot + "</td></tr>"
     else :
-        html += "<tr><td>No one<td></tr>"
+        html += "none</td></tr>"
+    # OSes
+    com = """DEVFILE=$(mktemp);MNTDIR=$(mktemp -d);HF=$MNTDIR/etc/sysconfig/network;fdisk -l | grep "Linux$" | awk '{print $1}'>$DEVFILE;while read line;do echo -n "$line:";if mount $line $MNTDIR 2>/dev/null;then if [ -f $HF ];then echo `cat $HF | grep HOSTNAME | awk -F'=' '{print $2}'`;else echo;fi;umount $MNTDIR;else echo;fi; done < $DEVFILE;rm -f $DEVFILE;rmdir $MNTDIR>/dev/null"""
+    html += "<tr class='detail1'><td><b>OSes</b></td><td>"
+    sin,sout,serr=ssh.exec_command(com)
+    oses = sout.read().splitlines()
+    print oses
+    for line in oses :
+        alist = re.split(":",line)
+        if alist[1] == "" :
+            continue
+        html += alist[0] + ":&nbsp;&nbsp;" + alist[1] + "<br>"
+    html += "</td></tr>"
+    # LOGIN
+    html += "<tr class='detail2'><td><b>LOGIN</b></td><td>"
+    sin,sout,serr=ssh.exec_command("who | awk '{print $5}'")
+    alist = sout.read().splitlines()
+    if alist :
+        who_dict = anawho(alist)
+        if who_dict :
+            for k in who_dict.keys() :
+                html += who_dict[k] + "&nbsp;&nbsp;From&nbsp;&nbsp;" + k + "<br>"
+            html += "</td></tr>"
+        else :
+            html += "No one</td></tr>"
+    else :
+        html += "No one</td></tr>"
     html += "</table>"
 
     return HttpResponse(html)
